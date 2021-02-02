5c921341b81abd6e61350cc5c063803dedab88c4
Reza sohrbi <rezasohrabi1995@yahoo.com>
2021-02-02 22:56:03 +0330

    webpack: asset management

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,31 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+      rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.(gif|png|svg|jpeg)$/i,
+            type: 'asset/reset'
+        },
+        {
+            test: /\.(woff|ttf|eot)/i,
+            type: 'asset/resource',
+        },
+        {
+            test: /\.csv$/i,
+            use: ['csv-loader'],
+        },
+        {
+            test: /\.xml$/i,
+            use: ['xml-loader'],
+        },
+      ],
+    },
 };
