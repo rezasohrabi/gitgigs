fd812776e32c52e393e9aed8fc34f8463266af93
Reza sohrbi <rezasohrabi1995@yahoo.com>
2021-02-04 13:27:31 +0330

    js methods : add replaceWord function

--- src/js-methods/strings.js
+++ src/js-methods/strings.js
@@ -1,4 +1,17 @@
 export default function runStringMethods() {    
-    console.log('hello module');
+    document.body.appendChild(replaceWord('reza'));
+    function replaceWord(word) {
+        const txt = "Definition and Usage\
+        The replace() method searches a string for a specified value, or a regular expression, and returns a new string where the specified values are replaced.\
+        Note: If you are replacing a value (and not a regular expression), only the first instance of the value will be replaced.\
+        To replace all occurrences of a specified value, use the global (g) modifier (see \"More Examples\" below).\
+        Read more about regular expressions in our RegExp Tutorial and our RegExp Object Reference.\
+        This method does not change the original string.";
+        const index = txt.indexOf("value");
+        const subStr = txt.slice(index, txt.length - 1);
+        const replacedStr = subStr.replace(/value/ig, word.toUppercase());
+        const span = document.createElement('span').appendChild(document.createTextNode(replacedStr));
+        return span;
+    }
 }
 
