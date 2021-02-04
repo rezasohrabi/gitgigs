6de9c5f3d04f33bcaacabbd0d24a5bd75a523835
Reza sohrbi <rezasohrabi1995@yahoo.com>
2021-02-04 15:07:28 +0330

    js methods: add strToList function

--- src/js-methods/strings.js
+++ src/js-methods/strings.js
@@ -1,5 +1,8 @@
-export default function runStringMethods() {    
+export default function runStringMethods() {   
+
     document.body.appendChild(replaceWord('reza'));
+    document.body.appendChild(strToList())
+
     function replaceWord(word) {
         const txt = "Definition and Usage\
         The replace() method searches a string for a specified value, or a regular expression, and returns a new string where the specified values are replaced.\
@@ -9,9 +12,28 @@ export default function runStringMethods() {
         This method does not change the original string.";
         const index = txt.indexOf("value");
         const subStr = txt.slice(index, txt.length - 1);
-        const replacedStr = subStr.replace(/value/ig, word.toUppercase());
-        const span = document.createElement('span').appendChild(document.createTextNode(replacedStr));
+        const replacedStr = subStr.replace(/value/ig, word.toUpperCase());
+        const span = document.createElement('span');
+        span.appendChild(document.createTextNode(replacedStr));
         return span;
+    }
+
+    function strToList() {
+        const txt = "The slice() Method"+
+        "slice() extracts a part of a string and returns the extracted part in a new string."+
+        "The method takes 2 parameters: the start position, and the end position (end not included)."+
+        "This example slices out a portion of a string from position 7 to position 12 (13-1):"+
+        "Remember: JavaScript counts positions from zero. First position is 0."+
+        "If a parameter is negative, the position is counted from the end of the string."+
+        "This example slices out a portion of a string from position -12 to position -6:";
+        const txtArr = txt.split('.');
+        const list = document.createElement('ul');
+        txtArr.forEach(function(item){
+            const li = document.createElement('li');
+            li.textContent = item;
+            list.appendChild(li);
+        }) 
+        return list;
     }
 }
 
