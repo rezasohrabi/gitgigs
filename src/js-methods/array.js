74e24c705da83ddf3d18e63df856a7cb6c4abd4e
Reza sohrbi <rezasohrabi1995@yahoo.com>
2021-02-04 18:34:24 +0330

    js methods: add findMax function to array module

--- src/js-methods/array.js
+++ src/js-methods/array.js
@@ -1,3 +1,26 @@
 export default function runArrayMethods() {
-    console.log('start array module')
+    
+    findMax([43,543,4234,3,2,1,96]);
+
+    function findMax(arr) {
+        let max = 0;
+
+        if(Array.isArray) 
+            if(!Array.isArray(arr)) return;
+        else if(arr.constructor.toString().indexOf('Array') === -1)  return 
+        // arr instanceof Array
+
+        for(let i = 0 ; i < arr.length ; i++){
+            compareArray(arr[i])
+        }
+
+        function compareArray(val) {
+            let num = Number(val);
+            if(num > max) max = num;
+            else return;
+        }
+
+        console.log('Max value is:', max);
+
+    }
 }
