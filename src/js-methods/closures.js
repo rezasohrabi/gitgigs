1bd3eac1837090e2499bd90c6ff0717b25b6392e
Reza sohrbi <rezasohrabi1995@yahoo.com>
2021-02-05 13:49:51 +0330

    js closures: add some simple closure function

--- src/js-methods/closures.js
+++ src/js-methods/closures.js
@@ -1,3 +1,33 @@
 export default function runClosureMethods() {
-console.log('hello closure');
+
+    const pubVar = 'this public var is for doSomething function, but every function in global scope can modify it';
+    function doSomething() {
+        console.log(pubVar);
+    }
+    doSomething();
+
+
+    function makeFunction() {
+        const prtVar = 'this is private var';
+        function closure() {
+            console.log('closure have access to private [name] variable of makeFunction() ==>', prtVar);
+        }
+        closure();
+    }
+    makeFunction();
+
+
+    function outerFunc() {
+        var name = 'private var';
+
+        function closure() {
+            name = name.concat('+')
+            console.log('closure have acces to private var of outer func ==> ', name)
+        }
+        return closure;
+    }
+    var closure = outerFunc()
+    closure();
+    closure();
+    
 }
